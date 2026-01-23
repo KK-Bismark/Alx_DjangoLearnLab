c38b7bb363083f6946b0ceca38b604f121997d2a
KK-Bismark <kbismarkkwaku@gmail.com>
2026-01-23 11:18:28 +0000

    rectified a module import issue.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,11 +4,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-        path('login/', LoginView.as_view(
-            template_name='relationship_app/login.html'
+        path('login/', LoginView.as_view(template_name='relationship_app/login.html'
             ), name='login'),
-        path('logout/', LogoutView.as_view(
-        template_name='relationship_app/logout.html'),
+        path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'),
              name='logout'),
         path('register/', views.register, name='register'),
         path('books/', views.list_books, name='list_books'),
