30d2e26f071147fb4d49a45c3843c71d954f4f9e
KK-Bismark <kbismarkkwaku@gmail.com>
2026-01-23 12:57:55 +0000

    Solution to task 4

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+
 class UserProfile(models.Model):
     ROLE_CHOICES = [
             ("Admin", "Admin"),
@@ -27,6 +28,13 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+                ("can_add_book", "Can add book"),
+                ("can_change_book", "Can change book"),
+                ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,54 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import user_passes_test, permission_required
 
 
 # Create your views here.
+
+@permission_required("relationship_app.can_add_book", raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author = request.POST.get("author")
+        published_date = request.POST.get("published_date")
+
+        Book.objects.create(
+                title=title,
+                author=author,
+                published_date=published_date,
+        )
+        return redirect("list_book")
+
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required("relationship_app.can_change_book", raise_exception=True)
+def edit_book(request):
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.author = request.POST.get("author")
+        book.published_date = request.POST.get("published_date")
+        book.save()
+        
+        return redirect("list_book")
+
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+
+
+@permission_required("relationship_app.can_delete_book", raise_exception=True)
+def delete_book(request):
+    book = get_object_or_404(Book, id=book_id)
+
+    if request.method == "POST":
+        book.delete()
+
+        return redirect("list_book")
+
+    return render(request, "relationship_app/delete_book.html", {"book": book})
+
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
