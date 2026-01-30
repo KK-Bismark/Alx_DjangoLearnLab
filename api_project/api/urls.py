2baa03ced0dd2b95f256d89d21f37b31c00e4df9
KK-Bismark <kbismarkkwaku@gmail.com>
2026-01-30 22:55:35 +0000

    Solution to task 1 - Complete

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
+from .views import BookList
 from django.urls import path
 
 
 urlpatterns = [
-   path('books/', BookList.as_view(), name='book-list'), # Maps to BookList view 
+   path('books/', BookList.as_view(), name='book-list'), # Maps to BookList view
 ]
