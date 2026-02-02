ad2a125d832c8172e356956057c9f53703d25aef
KK-Bismark <kbismarkkwaku@gmail.com>
2026-02-02 15:05:27 +0000

    Module error corrected

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,12 +2,14 @@ from .models import Book
 from .serializers import BookSerializer
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
